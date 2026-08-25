d3d07c2f9e0187d570b881b9f33cdf51eeb1eed0
h375569 <h375569@stud.u-szeged.hu>
2026-08-25 16:58:04 +0200

    aifix2

--- Express/src/services/ai.service.js
+++ Express/src/services/ai.service.js
@@ -154,7 +154,7 @@ const deleteEventsTool = {
 
 
 const fallbackModels = [
-  //'gemini-flash-latest',
+  'gemini-flash-latest',
   'gemini-3.6-flash',
   'gemini-3.5-flash',
   'gemini-3.5-flash-lite'

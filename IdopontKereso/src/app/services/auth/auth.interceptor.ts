b4276f9fb0b4c0db6ddd08143fe531d1dd4ff63d
h375569 <h375569@stud.u-szeged.hu>
2026-08-28 23:19:39 +0200

    ujabb proba auth fixre

--- IdopontKereso/src/app/services/auth/auth.interceptor.ts
+++ IdopontKereso/src/app/services/auth/auth.interceptor.ts
@@ -1,9 +1,8 @@
 import { HttpInterceptorFn, HttpRequest, HttpHandlerFn, HttpErrorResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from './auth.service';
-import { catchError, EMPTY, switchMap, throwError } from 'rxjs';
+import { catchError, switchMap, throwError } from 'rxjs';
 import { AuthResponse } from '../../models/auth.model';
-
 
 export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
@@ -21,29 +20,29 @@ export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
   }
 
   return next(authReq).pipe(
-  catchError((error) => {
-    if (error instanceof HttpErrorResponse && (error.status === 401 || error.status === 403)) {
-      
-      return authService.refreshToken().pipe(
-        switchMap((res: AuthResponse) => {
-          
-          if (res && res.accessToken) {
-            const retryReq = req.clone({
-              setHeaders: { Authorization: `Bearer ${res.accessToken}` }
-            });            
-            return next(retryReq);
-          }
-          
-          authService.logout();
-          return EMPTY;
-        }),
-        catchError((refreshErr) => {
-          authService.logout();
-          return EMPTY;
-        })
-      );
-    }
-    return throwError(() => error);
-  })
-);
+    catchError((error) => {
+      if (error instanceof HttpErrorResponse && (error.status === 401 || error.status === 403)) {
+        
+        return authService.refreshToken().pipe(
+          switchMap((res: AuthResponse) => {
+            
+            if (res && res.accessToken) {
+              const retryReq = req.clone({
+                setHeaders: { Authorization: `Bearer ${res.accessToken}` }
+              });            
+              return next(retryReq);
+            }
+            
+            authService.logout();
+            return throwError(() => new Error('Sikertelen token frissítés'));
+          }),
+          catchError((refreshErr) => {
+            authService.logout();
+            return throwError(() => refreshErr);
+          })
+        );
+      }
+      return throwError(() => error);
+    })
+  );
 }

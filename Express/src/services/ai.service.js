166f4eee8b951774d0bb21f944a23337b9a83486
h375569 <h375569@stud.u-szeged.hu>
2026-09-01 10:43:39 +0200

    ai modellek hozzaadasa

--- Express/src/services/ai.service.js
+++ Express/src/services/ai.service.js
@@ -196,10 +196,14 @@ const findAvailableTimeTool = {
 
 
 const fallbackModels = [
-  //'gemini-flash-latest',
+  'gemini-flash-latest',
   'gemini-3.6-flash',
   'gemini-3.5-flash',
-  'gemini-3.5-flash-lite'
+  'gemini-3.5-flash-lite',
+  'gemini-3-flash',
+  'gemini-3.1-flash-lite',
+  'gemini-2.5-flash',
+  'gemini-2.5-flash-lite',
 ];
 
 const getModel = (modelName) => {

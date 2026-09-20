297c8aef4e9fa0599adacf707435e0e2280dc384
h375569 <h375569@stud.u-szeged.hu>
2026-09-20 17:21:16 +0200

    backend api

--- IdopontKereso/src/environments/environment.ts
+++ IdopontKereso/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://api.useplanit.hu'
-  apiUrl: 'https://planit-backend-tukr.onrender.com'
+  apiUrl: 'https://api.useplanit.hu'
 };
